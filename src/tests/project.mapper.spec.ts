7a375f4ee02e8ef1a6a1eb9dd35cc433dc342c05
Manuel Gonzalez <manuelmaingonzalez@gus.global>
2021-03-11 19:02:57 +0100

    mapper specs added

--- src/tests/project.mapper.spec.ts
+++ src/tests/project.mapper.spec.ts
@@ -1,5 +1,22 @@
+import * as apiModel from 'pods/project/api/project.api-model';
+import * as viewModel from 'pods/project/project.vm';
+import { mapProjectFromApiToVm } from 'pods/project/project.mapper';
+
 describe('project.mapper specs', () => {
-  it('should pass spec', () => {
-    expect(true).toBeTruthy();
+  it('should return empty project when it feeds undifined', () => {
+    //Arrange
+    const project: apiModel.Project = undefined;
+    //Act
+    const result: viewModel.Project = mapProjectFromApiToVm(project);
+    //Assert
+    expect(result).toEqual(viewModel.createEmptyProject());
+  });
+  it('should return empty project when it feeds null', () => {
+    //Arrange
+    const project: apiModel.Project = null;
+    //Act
+    const result: viewModel.Project = mapProjectFromApiToVm(project);
+    //Assert
+    expect(result).toEqual(viewModel.createEmptyProject());
   });
 });
