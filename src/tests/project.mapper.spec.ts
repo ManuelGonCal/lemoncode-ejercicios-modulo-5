f3e7972b5d4a4c4ef64f44aa7322bf0f9d47f79c
Manuel Gonzalez <manuelmaingonzalez@gus.global>
2021-03-11 19:52:10 +0100

    mapper specs updated and working

--- src/tests/project.mapper.spec.ts
+++ src/tests/project.mapper.spec.ts
@@ -3,7 +3,7 @@ import * as viewModel from 'pods/project/project.vm';
 import { mapProjectFromApiToVm } from 'pods/project/project.mapper';
 
 describe('project.mapper specs', () => {
-  it('should return empty project when it feeds undifined', () => {
+  it('should return empty viewModel.project when it feeds undifined', () => {
     //Arrange
     const project: apiModel.Project = undefined;
     //Act
@@ -11,12 +11,31 @@ describe('project.mapper specs', () => {
     //Assert
     expect(result).toEqual(viewModel.createEmptyProject());
   });
-  it('should return empty project when it feeds null', () => {
+  it('should return empty viewModel.project when it feeds null', () => {
     //Arrange
     const project: apiModel.Project = null;
     //Act
     const result: viewModel.Project = mapProjectFromApiToVm(project);
     //Assert
     expect(result).toEqual(viewModel.createEmptyProject());
+  });
+  it('should return a viewModel.project when it feeds a apiModel.project', () => {
+    //Arrange
+    const project: apiModel.Project = {
+      id: '1',
+      name: 'test',
+      isActive: true,
+      employees: [],
+    };
+    const expectedResult: viewModel.Project = {
+      id: '1',
+      name: 'test',
+      isActive: true,
+      employees: [],
+    };
+    //Act
+    const result: viewModel.Project = mapProjectFromApiToVm(project);
+    //Assert
+    expect(result).toEqual(expectedResult);
   });
 });
